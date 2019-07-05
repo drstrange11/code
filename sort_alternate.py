d53f319a9e37725bfe2b2cb5d30f6ea5adae0f02
Ajay R <ajaysrikrishna@gmail.com>
2019-07-05 07:33:44 +0530

    Create sort_alternate.py

---
+++ sort_alternate.py
@@ -0,0 +1,12 @@
+#Problem Link - https://www.guvi.in/codekata-level?level=hunter&set=14
+
+n = int(input())
+l = list(map(int,input().split()))
+l.sort(reverse=True)
+le = len(l)
+ne = []
+for i in range(le//2+1):
+    ne.append(l[i])
+    ne.append(l[le-i-1])
+for x in ne[:len(ne)-1]:
+    print(x,end=" ")
