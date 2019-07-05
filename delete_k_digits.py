c5ae4271a74effffc5738c60e9e65c01053ad47b
Ajay R <ajaysrikrishna@gmail.com>
2019-07-05 08:47:48 +0530

    Create delete_k_digits.py

---
+++ delete_k_digits.py
@@ -0,0 +1,16 @@
+#Problem Link - https://www.guvi.in/codekata-level?level=pro&set=1 , 2
+
+num,k = map(int,input().split())
+num = list(str(num))
+stack = []
+for digit in num:
+    if k>0 and stack and stack[-1]>digit:
+        stack.pop()
+        k-=1
+    stack.append(digit)
+
+while k>0:
+    stack.pop()
+    k-=1 
+stack = ''.join(x for x in stack)
+print(int(stack))
