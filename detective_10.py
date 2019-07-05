0d8f6ad0af06e23a7f112f7230d40f0a182af678
Ajay R <ajaysrikrishna@gmail.com>
2019-07-05 09:05:16 +0530

    Create detective_10.py

---
+++ detective_10.py
@@ -0,0 +1,7 @@
+#Problem Link - https://www.guvi.in/codekata-level?level=pro&set=1,10
+n = int(input())
+l = list(map(int,input().split()))
+s = 0
+for x in l:
+    s = s+ (x*(x+1)//2) - x
+print(s)
