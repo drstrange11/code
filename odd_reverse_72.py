184a46bcc60d323675583611c8627c3e58ce43eb
Ajay R <ajaysrikrishna@gmail.com>
2019-07-05 11:37:04 +0530

    Update odd_reverse_72.py

--- odd_reverse_72.py
+++ odd_reverse_72.py
@@ -1,5 +1,9 @@
 #https://www.guvi.in/codekata-level?level=hunter&set=8, 72
-s = input().split()
+s = input()
+l = len(s)
+if s[l-1]=='.':
+    s = s[:-1]
+s = s.split()
 for i in range(len(s)):
     if (i+1)%2!=0:
         s[i] = s[i][::-1]
