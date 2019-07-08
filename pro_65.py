06bfbf370112450e56bda50486439a1c80ede60c
Ajay R <ajaysrikrishna@gmail.com>
2019-07-08 06:04:30 +0530

    Update pro_65.py

--- pro_65.py
+++ pro_65.py
@@ -1,9 +1,8 @@
-#Pro 65
 n,k= map(int,input().split())
 l = list(map(int,input().split()))
 price = int(input())
 val = (sum(l)-l[k])//2
-if price>val:
-    print(price-val)
-else:
+if price==val:
     print("Bon Appetit")
+else:
+    print(price-val)
