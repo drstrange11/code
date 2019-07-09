d52a00cfd96d2cd26c4d76459b5eb1294ff6a3f2
Ajay R <ajaysrikrishna@gmail.com>
2019-07-09 19:47:40 +0530

    Update pro_3.py

--- pro_3.py
+++ pro_3.py
@@ -6,11 +6,10 @@ def find(a,b,l_diff):
     return l_diff
 a,b = input().split()
 l_diff = abs(len(b)-len(a))
-#print(l_diff)
 if a==b:
     print(0)
 else:
-    if(len(a)>len(b)):
+    if len(a)>len(b):
         print(find(b,a,l_diff))
     else:
         print(find(a,b,l_diff))
