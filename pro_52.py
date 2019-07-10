b40dbcf2aec67c2a04ce43578163f268fdd1d269
Ajay R <ajaysrikrishna@gmail.com>
2019-07-10 06:03:41 +0530

    Create pro_52.py

---
+++ pro_52.py
@@ -0,0 +1,9 @@
+#Square
+a,b=map(int,input().split())
+c,d=map(int,input().split())
+e,f=map(int,input().split())
+g,h=map(int,input().split())
+if(abs((a-c)==(g-e))==abs((b-h)==(d-f))):
+    print("yes")
+else:
+    print("no")
