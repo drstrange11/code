6602aff591e6052a207b6e57d11df4c825c4f43f
Ajay R <ajaysrikrishna@gmail.com>
2019-07-12 16:52:40 +0530

    Update delete_k_digits.py

--- delete_k_digits.py
+++ delete_k_digits.py
@@ -3,7 +3,7 @@ num,k = map(int,input().split())
 num = list(str(num))
 stack = []
 for digit in num:
-    if k>0 and stack and stack[-1]>digit:
+    while k>0 and stack and stack[-1]>digit:
         stack.pop()
         k-=1
     stack.append(digit)
