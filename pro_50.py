c56af69f440eb74f3ea021aa953e903a82abb0ee
Ajay R <ajaysrikrishna@gmail.com>
2019-07-19 06:15:06 +0300

    Create pro_50.py

---
+++ pro_50.py
@@ -0,0 +1,21 @@
+m,n=map(int,input().split())
+l=[]
+for i in range(n):
+    l.append(list(map(int,input().split())))
+mod=10000000
+f=0
+for i in range(n):
+  if l[i][0]==1:
+    s=l[i][1]
+    c=l[i][2]
+    for j in range(i+1,n):
+      if l[j][0]==s:
+        s=l[j][1]
+        c+=l[j][2]
+    if c< and s==m:
+      =c
+      f+=1
+if f==0:
+  print(-1)
+else:
+  print()
