176c1db6013dc9071566e196aea3d808b793f997
Ajay R <ajaysrikrishna@gmail.com>
2019-07-19 06:04:56 +0300

    Update pro_43.py

--- pro_43.py
+++ pro_43.py
@@ -1,22 +1,22 @@
 #Lexological Permutation
-n = int(input())
-l = list(map(int,input().split()))
+n1 = int(input())
+a1 = list(map(int,input().split()))
 c,l = 0,[]
-b = [x for x in range(1,n+1)]
-for i in l:
-    if i in b:
-        b.remove(i)
+b = [x for x in range(1,n1+1)]
+for i in a1:
+  if i in b:
+    b.remove(i)
 d = 0
-for i in range(0,n-1):
-    p = l[i]
-    for j in range(i+1,n):
-        if p == l[j]:
-            if p < b[d]:
-                l[j] = b[d]
-                c += 1
-        else:
-            l[i] = b[d]
-            c += 1
-        d += 1
+for i in range(0,n1-1):
+  p = a1[i]
+  for j in range(i+1,n1):
+    if p == a1[j]:
+      if p < b[d]:
+        a1[j] = b[d]
+        c += 1
+      else:
+        a1[i] = b[d]
+        c += 1
+      d += 1
 print(c)
-print(*l)
+print(*a1)
