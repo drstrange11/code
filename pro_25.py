0b6098f05f5fda9e30993699fc9d139eec6126dc
Ajay R <ajaysrikrishna@gmail.com>
2019-07-19 06:22:06 +0300

    Create pro_25.py

---
+++ pro_25.py
@@ -0,0 +1,17 @@
+#Longest Increasing Subarray
+n=int(input())
+m=input()
+o=m.split(" ")
+p=[]
+for i in o:
+    p.append(int(i))
+q=[]
+count=1
+for j in range(0,n-1):
+    if p[j]<p[j+1]:
+        count+=1
+    else:
+        q.append(count)
+        count=1
+q.append(count)
+print(max(q))
