0f497044089a23c1ef0f69bc35b50d40161232af
Ajay R <ajaysrikrishna@gmail.com>
2019-07-19 06:10:56 +0300

    Update pro_47.py

--- pro_47.py
+++ pro_47.py
@@ -5,15 +5,15 @@ m=int(s[0])
 s=[int(d) for d in s[0]]
 h=0
 c=1;
-while(h==0):
+while h==0:
 	prod=c*m
-	k=[ int(d) for d in str(prod)]
+	k=[int(d) for d in str(prod)]
 	r=0
 	j=len(k)-1
-	while(k[j]==0):
+	while k[j]==0:
 		r+=1
 		j-=1
-	if(r>=g):
+	if r>=g:
 		print(prod)
 		h=1
 	c+=1
