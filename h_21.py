3fc7d64c6884ea9e03b11600b34c0c61a1eeac17
Ajay R <ajaysrikrishna@gmail.com>
2019-07-25 23:22:02 +0300

    Update h_21.py

--- h_21.py
+++ h_21.py
@@ -1,18 +1,18 @@
 N,K=map(int,input().split())
-l=[]
+xx=[]
 l1=[]
 for i in range(N):
-    l=[int(l) for l in input().split()]
-    l.append(l)
+    l=[int(xx) for xx in input().split()]
+    xx.append(l)
     if 0 in l:
         m=l.index(0)
         l1.append(m)
-for i in range(len(l)):
-    if 0 in l[i]:
+for i in range(len(xx)):
+    if 0 in xx[i]:
         for j in range(K):
-            l[i][j]=0
+            xx[i][j]=0
 for i in l1:
     for j in range(N):
-        l[j][i]=0
-for i in l:
+        xx[j][i]=0
+for i in xx:
     print(*i)
