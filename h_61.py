577d8eb2d8cc706a80e8eacd986e71c4c4261348
Ajay R <ajaysrikrishna@gmail.com>
2019-07-25 23:45:14 +0300

    Update h_61.py

--- h_61.py
+++ h_61.py
@@ -1,18 +1,10 @@
-n,k=map(int,input().split())
-l=[]
-l1=[]
-for i in range(n):
-    l=[int(l) for l in input().split()]
-    l.append(l)
-    if 0 in l:
-        m=l.index(0)
-        l1.append(m)
-for i in range(len(l)):
-    if 0 in l[i]:
-        for j in range(k):
-            l[i][j]=0
-for i in l1:
-    for j in range(n):
-        l[j][i]=0
-for i in l:
-    print(*i)
+n=int(input())
+li=list(map(int,input().split()))
+u,v=map(int,input().split())
+res=[]
+for i in range(0,len(li)-1):
+  for j in range(i+1,len(li)):
+    subarr=li[i:j+1]
+    if (subarr[0]==u and subarr[-1]==v) or (subarr[0]==v and subarr[-1]==u):
+      res.append(len(subarr))
+print(min(res)-1)
