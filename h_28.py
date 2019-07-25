27e2dd7e21fd66259243325ea5603e5b244bd21b
Ajay R <ajaysrikrishna@gmail.com>
2019-07-25 23:28:26 +0300

    Update h_28.py

--- h_28.py
+++ h_28.py
@@ -1,11 +1,12 @@
-s=int(input())
+#String 28
+S=int(input())
 n2=list(map(int,input().split()))
-l=[]
-l.append(sum(n2))
-for i in range(0,s-1):
+L=[]
+L.append(sum(n2))
+for i in range(0,S-1):
   a,a1=n2[:i+1],n2[i+1:]
   if sum(a)>sum(a1):
-    l.append(sum(a))
+    L.append(sum(a))
   else:
-    l.append(sum(a1))
-print(max(l))
+    L.append(sum(a1))
+print(max(L))
