89063e58b7e16a6dc670aa5e7f3646200dc167ca
Ajay R <ajaysrikrishna@gmail.com>
2019-08-13 20:48:50 +0530

    Update pro_69.py

--- pro_69.py
+++ pro_69.py
@@ -1,11 +1,13 @@
-#69
-n,k,m=map(int,input().split())
-b=list(map(int,input().split()))
-a=list(map(int,input().split()))
-a.sort()
-b.sort()
-l=[]
-for i in range(n):
-    l.append(m-b[i])
-    l.append(abs(a[i]-b[i]) + (a[i]-m))
-print(max(l))
+n,k,p = map(int,input().split())
+a = list(map(int,input().split()))
+a = sorted(a)
+b = list(map(int,input().split()))
+b = sorted(b)
+ 
+ans = int(2e9+1)
+for i in range(k-n+1):
+    tmp = 0
+    for j in range(n):
+        tmp = max(tmp,abs(a[j]-b[j+i])+abs(b[j+i]-p))
+    ans = min(ans,tmp)
+print(ans)
