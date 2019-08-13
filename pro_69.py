8431164d9caa78ee025391610be9641608a549c0
Ajay R <ajaysrikrishna@gmail.com>
2019-08-13 20:36:40 +0530

    Update pro_69.py

--- pro_69.py
+++ pro_69.py
@@ -1,11 +1,11 @@
 #69
 n,k,m=map(int,input().split())
-a=list(map(int,input().split()))
 b=list(map(int,input().split()))
+a=list(map(int,input().split()))
 a.sort()
 b.sort()
 l=[]
 for i in range(n):
     l.append(m-b[i])
-    l.append(abs(a[i]-b[i]) +(a[i]-m))
+    l.append(abs(a[i]-b[i]) + (a[i]-m))
 print(max(l))
