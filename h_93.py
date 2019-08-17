275b28a29d215fedaa7dc354deb0fb93e913fe5f
Ajay R <ajaysrikrishna@gmail.com>
2019-08-17 14:51:08 +0530

    Update h_93.py

--- h_93.py
+++ h_93.py
@@ -3,12 +3,12 @@ N=input()
 temp=""
 k=0
 for i in range(len(N)):
-  ik N[i]==" ":
+  if N[i]==" ":
     temp+=N[i]
-  elik k==0:
+  elif k==0:
     temp+=N[i].upper()
     k=1
-  elik k==1:
+  elif k==1:
     temp+=N[i]
     k=0
 print(temp)
