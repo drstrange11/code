b09aad59d9828f75d39b5d4e8a34d00ae788640e
Ajay R <ajaysrikrishna@gmail.com>
2019-08-17 19:16:38 +0530

    Update player_5.py

--- player_5.py
+++ player_5.py
@@ -1,8 +1,8 @@
 #AJ_5
 l1=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
-l1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+l2=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 s=input()
 for i in range(20):
   if s==l1[i]:
-    print(l1[i])
+    print(l2[i])
     break
